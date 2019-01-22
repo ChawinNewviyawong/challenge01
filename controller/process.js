21799d8f19708af48e483a1d0a4fb769239fce13
Chawin Newviyawong <N.Chawin@gmail.com>
2019-01-22 13:07:23 +0700

    challenge

--- controller/process.js
+++ controller/process.js
@@ -1,4 +1,44 @@
 function call(operator, operand1, operand2) {
-    return 0;
+    var result;
+    switch (operator) {
+        case "+":
+            result = operand1 + operand2;
+            break;
+        case "-":
+            result = operand1 - operand2;
+            break;
+        case "*":
+            result = operand1 * operand2;
+            break;
+        case "/":
+            if (operand2 != 0) {
+                result = operand1 / operand2;
+            }
+            else {
+                result = null;
+            }
+            break;
+        case "DIV":
+            if (operand2 != 0) {
+                result = operand1 / operand2;
+                result = Math.round(result);
+            }
+            else {
+                result = null;
+            }
+            break;
+        default:
+            break;
+    }
+    result = result.toFixed(4);
+    return result;
 }
-module.exports = call;
+
+function log(time) {
+    
+}
+
+module.exports = {
+    call,
+    log
+}
--- router/index.js
+++ router/index.js
@@ -2,7 +2,9 @@ var controller = require('../controller/process')
 var app = require('express').Router();
 
 app.post('/cal', function (req, res, next) {
-    res.send('<h1>Hello Node.js</h1>');
+    var time = req.body.time;
+    var result = controller.call(req.body.operator, req.body.operand1, req.body.operand2);
+    res.send(result);
 });
 
 module.exports = app;
