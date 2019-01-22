b85d6910a79bc930dc50eac356288df844b72c56
Hor <kunakorn.put@gmail.com>
2019-01-22 16:11:09 +0700

    add validate

--- controller/process.js
+++ controller/process.js
@@ -3,22 +3,33 @@ function call(operator, operand1, operand2) {
     var result;
     var resultFloat;
     var resultSeientific;
+    var code;
     // var numOperand1 = Number(operand1);
     // var numOperand2 = Number(operand2);
     
     switch (operator) {
         case "+":
+        operand1 = Number(operand1);
+        operand2 = Number(operand2);
             result = operand1 + operand2;
+            resultFloat = Number(result.toFixed(4));
+            resultSeientific = resultFloat.toExponential().replace(/e\+?/, ' x 10^')
             break;
         case "-":
             result = operand1 - operand2;
+            resultFloat = Number(result.toFixed(4));
+            resultSeientific = resultFloat.toExponential().replace(/e\+?/, ' x 10^')
             break;
         case "*":
             result = operand1 * operand2;
+            resultFloat = Number(result.toFixed(4));
+            resultSeientific = resultFloat.toExponential().replace(/e\+?/, ' x 10^')
             break;
         case "/":
             if (operand2 != 0) {
                 result = operand1 / operand2;
+                resultFloat = Number(result.toFixed(4));
+                resultSeientific = resultFloat.toExponential().replace(/e\+?/, ' x 10^')
             }
             else {
                 result = null;
@@ -28,6 +39,8 @@ function call(operator, operand1, operand2) {
             if (operand2 != 0) {
                 result = operand1 / operand2;
                 result = Math.round(result);
+                resultFloat = Number(result.toFixed(4));
+                resultSeientific = resultFloat.toExponential().replace(/e\+?/, ' x 10^')
             }
             else {
                 result = null;
@@ -36,24 +49,27 @@ function call(operator, operand1, operand2) {
         case "MOD":
             if (operand2 != 0) {
                 result = operand1 % operand2;
-                
+                resultFloat = Number(result.toFixed(4));
+                resultSeientific = resultFloat.toExponential().replace(/e\+?/, ' x 10^')
             }
             else {
                 result = null;
             }
             break;
         default:
+            code=400;
             break;
     }
-    resultFloat = Number(result.toFixed(4));
-    resultSeientific = resultFloat.toExponential().replace(/e\+?/, ' x 10^')
-    return {resultFloat,resultSeientific};
+    
+    return {resultFloat,resultSeientific,code};
  
 }
 
 function log(time) {
     
 }
+
+
 
 module.exports = {
     call,
--- router/index.js
+++ router/index.js
@@ -2,8 +2,21 @@ var controller = require('../controller/process')
 var app = require('express').Router();
 
 app.post('/cal', function (req, res, next) {
+    
     var time = req.body.time;
     var result = controller.call(req.body.operator, req.body.operand1, req.body.operand2);
+    if (req.body.operand1 == "" || req.body.operand2 == ""){
+        res.status(400);
+        res.send("No Operand Value !!!!!!!!")
+    }
+    if (req.body.tx_id == ""){
+        res.status(400);
+        res.send("TX_ID can not null")
+    }
+    if (result.code == 400){
+        res.status(400);
+        res.send("Operator incorrect!!!!!!")
+    }
     res.status(201);
     
     //appendHeader(res)
@@ -12,6 +25,7 @@ app.post('/cal', function (req, res, next) {
         result2Seientific : result.resultSeientific,
         TX_ID : req.body.tx_id
     });
+    
 });
 
 module.exports = app;
