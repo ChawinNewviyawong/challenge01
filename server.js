48217e9b4cb9bc4cb8c5c60a1ba57e008c53df3b
Hor <kunakorn.put@gmail.com>
2019-01-23 13:06:46 +0700

    Add swagger local

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var port = process.env.port || 3000;
+const swagger    =process.env.SWAGGER
+
+const swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require(`./api/swagger.json`);
 
 var bodyParser = require('body-parser');
 // parse application/json
@@ -9,6 +13,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', require('./router/index'));
 
 app.listen(port, function() {
